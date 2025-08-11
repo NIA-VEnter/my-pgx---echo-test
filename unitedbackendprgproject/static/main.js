67ed15f212754b92a02c5ecbf7b0597af06583da
NIA <nia3721wv.gmail.com>
2025-08-11 18:34:55 +0400

    ver 0.1 js test

--- unitedbackendprgproject/static/main.js
+++ unitedbackendprgproject/static/main.js
@@ -1,3 +1,9 @@
-function login(){
-    alert("nahui idi");
+function trylogin(){
+    const inputLogin = document.getElementById("InputLogin");
+    const inputPassword = document.getElementById("InputPassword");
+
+    const inputLogValue = inputLogin.value;
+    const inputPassValue = inputPassword.value;
+
+    console.log(inputLogin, inputLogValue, inputPassword, inputPassValue);
 }
