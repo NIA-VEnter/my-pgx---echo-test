844896c277fee3cbf605246fbfd866dc0299e81e
NIA <nia3721wv.gmail.com>
2025-08-11 18:44:40 +0400

    ver 0.1.3 Javascript is work

---
+++ unitedbackendprgproject/goscripts/passcheck/pgx.go
@@ -0,0 +1,15 @@
+package passcheck
+
+/*
+import (
+	"context"
+	"log"
+
+	"github.com/NIA-VEnter/my-pgx---echo-test/tree/main/unitedbackendprgproject/connect"
+	"github.com/jackc/pgx/v5"
+	conn "main.go/connect"
+)
+
+func checkPassword(conn *pgx.Conn){
+
+}*/
